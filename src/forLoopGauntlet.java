27419f23e2b2154ed1722d240f0dd0c3633d8ac5
League Student <league@iMac-10.attlocal.net>
2019-09-10 19:59:00 -0700

    LG
    :bento:
    :carousel_horse:

---
+++ src/forLoopGauntlet.java
@@ -0,0 +1,35 @@
+
+public class forLoopGauntlet {
+public static void main(String[] args) {
+	
+	//for(int i = 0; i<777; i++) {
+	//	if(i%7 == 0) {
+		//	System.out.println(i);
+		}
+	}
+	//int h = 0; 
+	//for(int g = 2007; g<2020; g++) { 
+		
+	//System.out.println("In " +g+ " I was " + h + " years old");
+
+	//h++; 
+
+	//}
+	//for(int d = 0; d < 3; d++) {
+		for(int a = 0; a < 3; a++) {
+			System.out.println(d +" "+ a );
+		
+		}
+
+}
+System.out.println("-----------------------");
+for(int f = 1; f < 10; f+=3) {
+	for(int s = f; s < s+3; s++) {
+		
+		System.out.print(s +" ");
+		
+		
+	}
+}
+}
+}
